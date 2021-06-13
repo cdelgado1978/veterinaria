033593697c72cf37f5eba4df605f9a8084fc9de4
enmanuel67 <enmanuelsecreto@gmail.com>
2021-06-12 22:25:38 -0400

    controlador empleado agregado

---
+++ Controlador/EmpleadoDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Veterinaria.Controlador
+{
+    public class EmpleadoDTO
+    {
+        public int Id { get; internal set; }
+        public string Nombre { get; internal set; }
+        public string Apellido { get; internal set; }
+        public int UsuarioId { get; internal set; }
+        public int CargoId { get; internal set; }
+        public int Sueldo { get; internal set; }
+        public string Direccion { get; internal set; }
+        public object CiudadId { get; internal set; }
+        public string Telefono { get; internal set; }
+        public string Cedula { get; internal set; }
+        public string Correo { get; internal set; }
+        public bool Inactivo { get; internal set; }
+    }
+}
---
+++ Controlador/EmpleadosControlador.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veterinaria.Modelo;
+
+namespace Veterinaria.Controlador
+{
+    class EmpleadosControlador : IControlador<Empleado>
+    {
+        private readonly VeterinariaEntities db;
+
+        public EmpleadosControlador()
+        {
+            db = new VeterinariaEntities();
+        }
+
+        public Empleado Obtener(int id)
+        {
+            return db.Empleados.FirstOrDefault(e => e.ID == id);
+
+        }
+
+        public List<EmpleadoDTO> ObtenerTodos()
+        {
+            var _result = db.Empleados.ToList();
+
+            List<EmpleadoDTO> _empleado = new List<EmpleadoDTO>();
+
+            _result.ForEach(e =>
+            {
+                _empleado.Add(new EmpleadoDTO()
+                {
+                    Id = e.ID,
+                    Nombre = e.Nombre,
+                    Apellido = e.Apellidos,
+                    UsuarioId = e.UsuarioId,
+                    CargoId = e.CargoId,
+                    Sueldo = e.Sueldo,
+                    Direccion = e.Direccion,
+                    CiudadId = e.CiudadID,
+                    Telefono = e.Telefono,
+                    Cedula = e.Cedula,
+                    Correo = e.Correo,
+                    Inactivo = e.Inactivo,
+                    
+                    
+
+                });
+            });
+
+            return _empleado;
+        }
+
+        public Empleado Agregar(Empleado entidad)
+        {
+            if (entidad != null)
+            {
+                db.Empleados.Add(entidad);
+                db.SaveChanges();
+
+
+            }
+
+            return entidad;
+
+        }
+
+        public void Editar(Empleado entidad)
+        {
+            //if (entidad == null) throw new Exception("Entidad Invalidad");
+
+            var empleado = db.Empleados.Find(entidad);
+            if (empleado != null)
+            {
+                db.Empleados.AddOrUpdate(entidad);
+                db.SaveChanges();
+            }
+        }
+
+        public void Borrar(Empleado entidad)
+        {
+            //if (entidad == null) throw new Exception("Entidad Invalidad");
+
+            var empleado = db.Empleados.Find(entidad);
+            if (empleado != null)
+            {
+                empleado.Inactivo = true;
+
+                db.Empleados.AddOrUpdate(empleado);
+                db.SaveChanges();
+            }
+
+        }
+
+        
+    }
+}
+
