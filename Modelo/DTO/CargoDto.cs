919c68961f816783979115942d2e8f36af2617e3
Cesar Delgado <cesardelgado73@hotmail.com>
2021-06-13 20:40:01 -0400

    se movio CargoDTO

---
+++ Modelo/DTO/CargoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Veterinaria.Modelo.DTO
+{
+    public class CargoDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public bool Inactivo { get; set; }
+    }
+}
